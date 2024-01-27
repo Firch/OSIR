bf811180485170d924f4d761edea5021d92df937
Firch <47253246+Firch@users.noreply.github.com>
2024-01-28 03:31:10 +0500

    Even better windows - proper focus

--- scenes/ui/windows/window.gd
+++ scenes/ui/windows/window.gd
@@ -7,7 +7,7 @@ extends Panel
 @onready var CloseButton = $CloseButtonMargins/CloseButton
 @onready var CloseButtonSprite = $CloseButtonMargins/CloseButton/CloseButtonSprite
 
-var focused: bool = true # Is it in focus?
+var focused: bool = false # Is it in focus?
 var mouseover: bool = false # Is the mouse over the window's dragborder?
 var moveable: bool = true # Can it be moved?
 var moving: bool = false # Does it move currently?
@@ -17,33 +17,40 @@ func _ready():
 	# Title:
 	if Title.text != null:
 		Title.text = Text
+		
+		
 	# Connects:
 	DragBorder.button_down.connect(_on_start_drag)
 	DragBorder.button_up.connect(_on_end_drag)
-	focus_entered.connect(_on_focus)
-	focus_exited.connect(_on_unfocus)
+	
+	focus_entered.connect(focus)
+	focus_exited.connect(unfocus)
+	
+	# Very cool loop that adds focusing-unfocusing for every clickable:
+	for clickable in get_tree().get_nodes_in_group("Clickables"):
+		if is_ancestor_of(clickable):
+			clickable.focus_entered.connect(focus)
+			clickable.focus_exited.connect(unfocus)
+	
 	CloseButton.button_down.connect(_on_closebutton_down)
 	CloseButton.button_up.connect(_on_closebutton_up)
 	CloseButton.pressed.connect(_on_close)
+	
+	
+	# Unfocus:
+	if !focused:
+		unfocus()
 
 
 func _on_start_drag():
 	if moveable:
 		moving = true
-		move_to_front()
+		focus()
 
 
 func _on_end_drag():
 	if moveable:
 		moving = false
-
-
-func _on_focus():
-	focus()
-
-
-func _on_unfocus():
-	unfocus()
 
 
 func _on_closebutton_down():
@@ -61,11 +68,13 @@ func _on_close():
 
 func focus():
 	focused = true
+	set_theme_type_variation("Focused")
 	move_to_front()
 
 
 func unfocus():
 	focused = false
+	set_theme_type_variation("Unfocused")
 
 func _input(event):
 	if event is InputEventMouseMotion and moving == true:
