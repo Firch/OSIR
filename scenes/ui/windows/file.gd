0e155df21f3a0e5f439dd29647d851f9754e6255
Firch <47253246+Firch@users.noreply.github.com>
2024-02-27 16:33:50 +0500

    SubViewport folder (very bad)

--- scenes/ui/windows/file.gd
+++ scenes/ui/windows/file.gd
@@ -1,11 +1,54 @@
 extends Control
 
+#
+# This is JANK (I think). Please rewrite this.
+#
 
-# Called when the node enters the scene tree for the first time.
+@export var File: Script
+
+@onready var FileButton = $Button
+@onready var Selection = $Selection
+@onready var ClickTimer = $Button/ClickTimer
+
+var enabled: bool = true # Can be interacted with? Change that when dragging
+var ghost: bool = false # Is it attached to cursor?
+
+var prepared: bool = false # Will be true when it is just pressed, but will return false after Doubleclick timer finishes
+
 func _ready():
-	pass # Replace with function body.
+	# Connects:
+	FileButton.pressed.connect(_on_click)
+	FileButton.focus_entered.connect(_on_focus)
+	FileButton.focus_exited.connect(_on_unfocus)
+	ClickTimer.timeout.connect(_on_click_timeout)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_focus():
+	Selection.visible = true
+
+
+func _on_unfocus():
+	Selection.visible = false
+	# Selection.visible = false
+
+
+func _on_click():
+	if enabled:
+		
+		if prepared == true:
+			run()
+		
+		prepared = true
+		ClickTimer.start()
+
+
+func _on_click_timeout(): # Resets double click
+	prepared = false
+
+
+func run(): # Open the file
+	print("Run")
